4102499813714071c60995c1e817ef81d01011ec
Phil Knowles <fighters4christ@outlook.com>
2018-05-11 17:15:15 -0500

    - Added import, export, package

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
+from import_export import resources, fields
 
+# Third Party
+from import_export.admin import ImportExportModelAdmin
+
+# App
 from .models import Task
 
+
 # Register your models here.
-admin.site.register(Task)
+class TaskResource(resources.ModelResource):
+    class Meta:
+        model = Task
+
+
+class TaskAdmin(ImportExportModelAdmin):
+    resource_class = TaskResource
+
+
+admin.site.register(Task, TaskAdmin)
+
